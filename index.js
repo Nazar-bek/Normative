dedaa4342da52b02922a203e7af504dfb7132c31
Nazar-bek <nazarsayfullayev@gmail.com>
2025-07-16 18:37:35 +0500

    another change

--- index.js
+++ index.js
@@ -259,3 +259,21 @@ window.addEventListener("click", (e) => {
     document.body.style.overflow = "auto";
   }
 });
+
+
+const upBtn = document.querySelector(".up__button")
+
+window.addEventListener("scroll", () =>{
+  if(window.scrollY > 500){
+    upBtn.classList.add("active")
+  }else{
+    upBtn.classList.remove("active")
+  }
+})
+
+upBtn.addEventListener("click",() =>{
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+})
